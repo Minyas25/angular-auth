295db6369ebecd0cb09b6a1f10ba373e33d6e604
Jean Demel <jean@m2i.fr>
2023-07-11 15:13:55 +0200

    added register form

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { User } from '../entities';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+  user:User = {email:'', password:''};
+  feedback = ''
+  constructor(private authService:AuthService){}
+
+  onSubmit() {
+    this.authService.addUser(this.user).subscribe(() => this.feedback ='Registration complete.');
+  }
+
+}
